e515fb22c35106fa5f9fbfd7bc1b7a4834128be7
emmamkb <emmanuelmakabu7@gmail.com>
2024-02-22 12:30:11 +0200

    Design homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-    <!-- Mirrored from creazione.avanzare.co/gravity/demo/index.html?bg=waves by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 13 Feb 2024 14:04:45 GMT -->
     <head>
         <meta charset="utf-8" />
         <meta name="robots" content="noodp" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <title>Avascent | Coming Soon</title>
-        <link rel="shortcut icon" href="assets/favicon.ico" />
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,600|Raleway:300,400" rel="stylesheet" />
         <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/asf2525xljt.css') }}" />
     </head>
@@ -31,9 +30,6 @@
                             </div>
                         </div>
                         <p>OUR WEBSITE IS COMING SOON</p>
-                        <div class="arrow-wrap go-down">
-                            <div class="open-overlay down-button"><i class="ti-arrow-down"></i> <i class="ti-arrow-down"></i></div>
-                        </div>
                     </div>
                 </div>
                 <div class="background-content page-enter-animated">
@@ -49,50 +45,6 @@
                     </div>
                 </div>
             </section>
-            <div id="overlay">
-                <section class="overlay">
-                    <div id="about" class="content">
-                        <div class="container-mid">
-                            <div class="container">
-                                <h1>OUR WEBSITE IS COMING SOON</h1>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
-                                    felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-                                    venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
-                                </p>
-                                <form action="https://creazione.avanzare.co/gravity/demo/assets/php/subscribe.php" method="post" class="subscribe-form">
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="input-group input-group-lg">
-                                                <input type="text" name="email" placeholder="* Enter your email to Subscribe!" class="email form-control" />
-                                                <div class="fhp-input"><input type="text" name="phone" placeholder="* Enter your phone to Subscribe!" class="email form-control" /></div>
-                                                <div class="input-group-btn">
-                                                    <button type="submit" class="btn btn-primary form-control"><i class="ti-arrow-right first"></i><i class="ti-arrow-right second"></i></button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                                <ul class="social-icons">
-                                    <li>
-                                        <a data-placement="bottom" title="Facebook" class="ti-facebook" href="#"><div class="overlay"></div></a>
-                                    </li>
-                                    <li>
-                                        <a data-placement="bottom" title="Twitter" class="ti-twitter-alt" href="#"><div class="overlay"></div></a>
-                                    </li>
-                                    <li>
-                                        <a data-placement="bottom" title="Google+" class="ti-google" href="#"><div class="overlay"></div></a>
-                                    </li>
-                                    <li>
-                                        <a data-placement="bottom" title="Contact" class="fa fa-envelope go-down" href="#"><div class="overlay"></div></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <div class="up-button go-up"><i class="ti-arrow-up"></i><i class="ti-arrow-up"></i></div>
-            </div>
         </div>
         <script type="text/javascript" src="assets/js/xsg235t4r1r.js"></script>
     </body>
